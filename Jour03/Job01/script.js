0adef289b4f479f57c66a107e4c78637cf22ee7e
michael.noiret <michael.noiret@laplateforme.io>
2026-03-26 10:18:34 +0100

    Ajout Jour03

---
+++ Jour03/Job01/script.js
@@ -0,0 +1,23 @@
+function hide() {
+$("#buttonhide").on("click", function() {
+    $("#text").hide(); // .fadeOut()
+    });
+}
+    // $("#texte").text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord, on les construit, ensuite, on prie.");
+    // if ($("#text").show()) {
+    
+
+function show() {
+$("#buttonshow").on("click", function() {
+    // if ($("#text").hide()) {
+    $("#text").show(); // .fadeIn()
+})
+}
+show();
+hide();
+
+// $("#buttonhide").click(function() {
+//     $("#text").toggle();
+// })
+
+
---
+++ Jour03/Job02/script.js
@@ -0,0 +1,7 @@
+let images = [images/arc1.png, images/arc2.png, images/arc3.png, images/arc4.png, images/arc5.png, images/arc6.png];
+$("mélange").on("click", function() {
+    $("images").unsort()
+
+    
+})
+
